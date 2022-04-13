2f3b91fe7f7ae18b0a9fe87deb41654ce4a3662a
t-Komori <t-komori@tokyoweld.com>
2022-04-13 15:46:29 +0900

    feature-2ブランチ作成
    ブランチを新規作成したときの確認

--- TwGitPractice/Program.cs
+++ TwGitPractice/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("feature / new branch take2");
 Console.WriteLine("feature commit 1");
 Console.WriteLine("master commit 1");
+Console.WriteLine("feature-2 / new branch");

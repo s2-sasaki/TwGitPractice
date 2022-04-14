8261647913de3175925039e45b0fe4cb18a8c932
t-Komori <t-komori@tokyoweld.com>
2022-04-14 14:43:00 +0900

    masterへプッシュ
    整理のため

--- TwGitPractice/Program.cs
+++ TwGitPractice/Program.cs
@@ -1,13 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("feature / new branch take2");
-Console.WriteLine("feature commit 1");
-Console.WriteLine("master commit 1");
-Console.WriteLine("feature-2 / new branch");
-Console.WriteLine("feature-2 commit 1");
-Console.WriteLine("feature-2 commit 2");
-Console.WriteLine("feature-2 commit 3");
-Console.WriteLine("feature-3 / new branch");
-Console.WriteLine("Pull Request Canceled");
-Console.WriteLine("Pull Request Canceled2");
-Console.WriteLine("Pull Request Canceled3");
+
